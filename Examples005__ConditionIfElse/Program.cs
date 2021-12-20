82310765c319a3f62f5cae553d6b7110b3ec343d
Elvira09 <zag0909@gmail.com>
2021-12-20 15:08:50 +0300

    Создали проект Examples005__ConditionIfElse/

---
+++ Examples005__ConditionIfElse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите имя пользователя: ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "elvira")
+{
+    Console.WriteLine("Ура, это же ELVIRA!");
+}
+else
+{
+    Console.Write("Привет, ");
+    Console.WriteLine(username);
+}
