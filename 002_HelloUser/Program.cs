c05960277e6e4caad45df35148d57bab9b1e7605
Elvira09 <zag0909@gmail.com>
2021-12-26 20:55:39 +0300

    cd

---
+++ 002_HelloUser/Program.cs
@@ -0,0 +1,7 @@
+﻿//Вывод на экран приветствиt пользователя
+
+Console.WriteLine("Введите Ваше имя  "); //
+string username = Console.ReadLine();
+
+Console.WriteLine("Привет,  ");
+Console.WriteLine(username);
