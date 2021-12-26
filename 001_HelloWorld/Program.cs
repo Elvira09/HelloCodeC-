a128d92bf56a0c94292fdf9319ba512d96001f94
Elvira09 <zag0909@gmail.com>
2021-12-26 16:03:17 +0300

    Вывод на экран приветствия Hello, World

--- 001_HelloWorld/Program.cs
+++ 001_HelloWorld/Program.cs
@@ -1,1 +1,6 @@
-﻿Console.WriteLine("2 Hello, World!");
+﻿//Вывод на экран приветствия Hello, World!
+
+
+Console.WriteLine("Hello, World!"); 
+    //Console - взаиомодействие с терминалом
+    //WriteLine("Hello, World!") - оператор для вывода в окно терминала информации (текст или значение )
