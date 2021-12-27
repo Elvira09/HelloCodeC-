7708391856fd30493336e21387fa16adc2ad904e
Elvira09 <zag0909@gmail.com>
2021-12-27 18:48:43 +0300

    cd

---
+++ 003_Sum/Program.cs
@@ -0,0 +1,9 @@
+﻿// Сумма случайных чисел от 1 до 9
+
+//int numberA = new Random().Next(1,10)
+int numberA = new Random().Next(1,10); //1 2 3 ...9
+Console.WriteLine(numberA);
+int numberB = new Random().Next(1,10); 
+Console.WriteLine(numberB);
+int result = numberA + numberB;
+Console.WriteLine (result);
